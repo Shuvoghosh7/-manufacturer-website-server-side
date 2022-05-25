059ab87e31fa3cc484043a1a1b08b33a4e65d111
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-25 23:44:51 +0600

    add api for confirm payment and shotr payment database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const app = express()
 
@@ -38,17 +39,18 @@ async function run() {
     const userCollection = client.db('parts-manufacturer').collection('users');
     const reviewCollection = client.db('parts-manufacturer').collection('review');
     const profileCollection = client.db('parts-manufacturer').collection('profile');
+    const paymentCollection = client.db('parts-manufacturer').collection('payment');
 
     console.log("Connect DB")
     // verifyAdmin function
-    const verifyAdmin = async(req, res, next)=>{
+    const verifyAdmin = async (req, res, next) => {
       const requester = req.decoded.email;
-          const requesterAccount = await userCollection.findOne({ email: requester });
-          if (requesterAccount.role === 'admin') {
-            next()
-          }else{
-            res.status(403).send({message: 'forbidden'});
-          }
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next()
+      } else {
+        res.status(403).send({ message: 'forbidden' });
+      }
     }
 
     app.get('/get-parts', async (req, res) => {
@@ -89,7 +91,7 @@ async function run() {
       res.send(users);
     });
     // make admin
-    app.put('/user/admin/:email', verifyJwt,verifyAdmin, async (req, res) => {
+    app.put('/user/admin/:email', verifyJwt, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const updateDoc = {
@@ -105,47 +107,84 @@ async function run() {
       const isAdmin = user.role === 'admin';
       res.send({ admin: isAdmin })
     })
-   // my order
-   app.get('/orders', async (req, res) => {
-    const email = req.query.email
-    const query = { email: email };
-    const cursor = orderCollection.find(query);
-    const result = await cursor.toArray();
-    res.send(result)
-  })
-  // Delete orders
-  app.delete('/orders/:id', async (req, res) => {
-    const id = req.params.id
-    const query = { _id: ObjectId(id) };
-    const result = await orderCollection.deleteOne(query)
-    res.send(result)
-  })
-   //add rewiew
-   app.post('/review', async(req,res)=>{
-    const review=req.body
-    const result=await reviewCollection.insertOne(review)
-    res.send(result)
-  });
-  // get review
-  app.get('/get-review', async (req, res) => {
-    const result = await reviewCollection.find({}).toArray()
-    res.send(result)
-  })
+    // my order
+    app.get('/orders', async (req, res) => {
+      const email = req.query.email
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    //single order
 
-  // my profile
-  app.post('/profile',verifyJwt, async(req,res)=>{
-    const profile=req.body
-    const result=await profileCollection.insertOne(profile)
-    res.send(result)
-  });
-  // get my profile data
-  app.get('/get-profile', async (req, res) => {
-    const email = req.query.email
-    const query = { email: email };
-    const cursor = profileCollection.find(query);
-    const result = await cursor.toArray();
-    res.send(result)
-  })
+    app.get('/order/:id', verifyJwt, async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await orderCollection.findOne(query)
+      res.send(result)
+    })
+    // Delete orders
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query)
+      res.send(result)
+    })
+    //add rewiew
+    app.post('/review', async (req, res) => {
+      const review = req.body
+      const result = await reviewCollection.insertOne(review)
+      res.send(result)
+    });
+    // get review
+    app.get('/get-review', async (req, res) => {
+      const result = await reviewCollection.find({}).toArray()
+      res.send(result)
+    })
+
+    // my profile
+    app.post('/profile', verifyJwt, async (req, res) => {
+      const profile = req.body
+      const result = await profileCollection.insertOne(profile)
+      res.send(result)
+    });
+    // get my profile data
+    app.get('/get-profile', async (req, res) => {
+      const email = req.query.email
+      const query = { email: email };
+      const cursor = profileCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    // payment method
+    app.post('/create-payment-intent', verifyJwt, async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({ clientSecret: paymentIntent.client_secret })
+    });
+
+    // update booking for payment
+    app.patch('/order/:id', verifyJwt, async(req, res) =>{
+      const id  = req.params.id;
+      const payment = req.body;
+      const filter = {_id: ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+
+      const result = await paymentCollection.insertOne(payment);
+      const updatedBooking = await orderCollection.updateOne(filter, updatedDoc);
+      res.send(updatedBooking);
+    })
   }
   finally {
 
